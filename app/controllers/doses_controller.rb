3cd8808f6fc95b8e4fdc16f22e3b2e04b9840ef1
HC Tung <hc.tung@gmail.com>
2020-08-21 12:20:40 +0200

    removed raise

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,7 +7,6 @@ class DosesController < ApplicationController
   def create
     @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
-    raise
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
